54a056081ceabba0b3dfad764d9ae2365075ca56
Michael Luongo <luongomichaelr@gmail.com>
2018-05-07 09:48:27 -0500

    update to Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -19,3 +19,30 @@ module.exports = {
     }]
   }
 }
+module: {
+
+    rules: [{
+
+         test: /\.js$/,
+
+         exclude: /node_modules/,
+
+         use: {
+
+             loader: 'babel-loader',
+
+            options: {
+
+                 presets: ['react']
+
+            }
+
+        }
+
+    }]
+
+},
+
+mode: 'development'
+
+}
