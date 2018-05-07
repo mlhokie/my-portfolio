bdad1892ebe6af854ad34b5a06ee3fcdab0285c9
Michael Luongo <luongomichaelr@gmail.com>
2018-05-07 13:56:54 -0500

    found spacing error

--- js/example-work.js
+++ js/example-work.js
@@ -25,7 +25,7 @@ class ExampleWorkBubble extends React.Component {
           <div className="section__example">
             <img alt={ example.image.desc }
                  className="section__exampleImage"
-                 src={ example.image.src}/>
+                 src={ example.image.src }/>
             <dl className="color--cloud">
               <dt className="section__exampleTitle section__text--centered">
                 { example.title }
