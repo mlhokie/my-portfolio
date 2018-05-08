d6f60a017edf8346c32b45ccebc47cd8621bf242
Michael Luongo <luongomichaelr@gmail.com>
2018-05-08 10:46:39 -0500

    Troubleshooting testing errors, still one test fail

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ExampleWork from './example-work';
+import ExampleWork from './example-work.js';
 
 const myWork = [
   {
     'title': "Certifications",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "example image of professional certification stamp",
       'src': "images/certifiedstamp.png",
@@ -13,6 +15,8 @@ const myWork = [
   },
   {
     'title': "Online Classes",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "example image of a schoolhouse",
       'src': "images/schoolhouse.png",
@@ -21,12 +25,14 @@ const myWork = [
   },
   {
     'title': "Interests",
+    'href': "https://example.com",
+    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
       'desc': "example screenshot of computer build",
       'src': "images/sidecomputer.png",
       'comment': ""
     }
   }
-]
+];
 
 ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
