23f1e36fd811a4f8a7d6b570a3c2f5d3bd620adc
Michael Luongo <luongomichaelr@gmail.com>
2018-05-10 15:02:54 -0500

    Added text to explain Responsive, and grammar check

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ import ExampleWork from './example-work.js';
 const myWork = [
   {
     'title': "Certifications",
-    'href': "http://127.0.0.1:8000/ResumeMichaelLuongo.pdf",
+    'href': "ResumeMichaelLuongo.pdf",
     'desc': "My collection is growing! So far I have obtained two Microsoft Technology Associate certificates and I have completed and passed both CompTIA A+ and CompTIA Network+. Achieving both of those gives me the title of CIOS (CompTIA IT Operations Specialist). Several other certifications are on the horizon. The next three I am looking to achieve are: AWS: Certified Solutions Architect (Associate), ITIL-Foundation, and CompTIA Security+.",
     'image': {
       'desc': "example image of professional certification stamp",
@@ -18,11 +18,11 @@ const myWork = [
   },
   {
     'title': "Education & Work Experience",
-    'href': "http://127.0.0.1:8000/ResumeMichaelLuongo.pdf",
+    'href': "ResumeMichaelLuongo.pdf",
     'desc': [`I received a BS degree in Education from Virginia Tech.
 Shortly after graduation, I was offered a postion in Marketing/Sales with Skechers USA as a Territory Manager for the Mid-Atlantic region of the US.
 After several years doing that I relocated to Charlotte, NC where I quickly became Assistant Branch Manager for Enterprise Rent-A-Car.`, `I returned to NJ, the area I grew up in, after awhile to return to teaching.
-After 2 years of substitute teaching and not having an opportunity for a permanent position I decided to become an Independent Contractor and work for myself, transporting RV's, boats, and horse trailers all around the USA and Canada
+After 2 years of substitute teaching and not having an opportunity for a permanent position I decided to become an Independent Contractor and work for myself, transporting RV's, boats, and horse trailers all around the USA and Canada.
  Several years in I really started to focus on preparing for a career in IT.`, `I've always had a passion for technology and the way it had evolved over the years. I started watching online videos and participating in online lectures.
 Right as 2018 started to roll in I decided to make a serious change and get formal training. I chose CCI Training center in Dallas, TX. There I have learned the foundation to IT. I was able to build on and fill in all the missing gaps
 in knowledge. Check out my Certifications page to view my certificates thus far.`,
@@ -38,7 +38,7 @@ in knowledge. Check out my Certifications page to view my certificates thus far.
   },
   {
     'title': "Interests",
-    'href': "http://127.0.0.1:8000/ResumeMichaelLuongo.pdf",
+    'href': "ResumeMichaelLuongo.pdf",
     'desc': [`One main interest of mine has been the fascination of building my own computers from just a bunch of parts ordered online and turning them into working machines. This is my most recent build. I started off with a mid-tower case. I added 4 case fans, 3 of which have LED lights. It is all built on an ASUS motherboard using a Ryzen 5-1600 6 core processor, and Wraith Spire cooler. For storage a Samsung, M.2 NVMe solid state drive is attached. I added some LED strips to the inside for some color effects.`, `Outside of school and work I have a miniature pinscher named Scooby. I love traveling and together, Scooby and I, have have been to the 48 contiguous states and almost all the Canadian provinces. I have also traveled overseas solo to Italy, France, Spain, and Vatican City`],
     'image': {
       'desc': "example screenshot of computer build",
